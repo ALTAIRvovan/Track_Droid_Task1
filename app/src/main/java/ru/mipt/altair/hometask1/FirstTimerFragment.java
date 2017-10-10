32f61cd73ff72a0175e66bbb652bddf7f7796338
Vladimir Panov <vladimir.panov@phystech.edu>
2017-10-10 22:10:48 +0300

    добавил finish для первого активити

--- app/src/main/java/ru/mipt/altair/hometask1/FirstTimerFragment.java
+++ app/src/main/java/ru/mipt/altair/hometask1/FirstTimerFragment.java
@@ -9,9 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class FirstTimerFragment extends TimerFragment {
 
     public FirstTimerFragment() {
@@ -20,19 +17,13 @@ public class FirstTimerFragment extends TimerFragment {
         TIMER_DURATION = 2000;
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_timer, container, false);
-    }
-
     @Override
     protected void onTimerFinish() {
         super.onTimerFinish();
 
         final Intent intent = new Intent(getActivity(), SecondActivity.class);
         startActivity(intent);
+
+        getActivity().supportFinishAfterTransition();
     }
 }
